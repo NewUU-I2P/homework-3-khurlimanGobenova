cd78349c97b6e355d464c155ac6bb89edd0daf95
Xurliman Gobenova <x.gobenova@newuu.uz>
2023-11-13 22:46:31 +0500

    test

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,23 +1,29 @@
 float problemSolution1(float consumed_water) {
-    float cost = 13;
-    if (consumed_water <= 30) {
-        cost = cost + consumed_water * 0.4;
-    } else {
-        cost = cost + 30 * 0.4;
-        consumed_water = consumed_water - 30;
-        if (consumed_water <= 20) {
-            cost = cost + consumed_water * 0.12;
-        }else{
-            consumed_water = consumed_water - 20;
-            cost = cost + 20*0.12;
-            if (consumed_water <=10) {
-                cost = cost + 1.4 * consumed_water;
+    float cost = 0;
+    while (consumed_water>0) {
+        cost = 13;
+        if (consumed_water <= 30) {
+            cost = cost + consumed_water * 0.4;
+            break;
+        } else {
+            cost = cost + 30 * 0.4;
+            consumed_water = consumed_water - 30;
+            if (consumed_water <= 20) {
+                cost = cost + consumed_water * 0.12;
+                break;
             }else{
-                consumed_water = consumed_water - 10;
-                cost = cost + 1.4*10;
+                consumed_water = consumed_water - 20;
+                cost = cost + 20*0.12;
+                if (consumed_water <=10) {
+                    cost = cost + 1.4 * consumed_water;
+                    break;
+                }else{
+                    consumed_water = consumed_water - 10;
+                    cost = cost + 1.4*10;
+                }
             }
         }
+        cost = cost + consumed_water * 1.5;
     }
-    cost = cost + consumed_water * 1.5;
     return cost;
 }
